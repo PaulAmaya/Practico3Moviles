bbd693058847a053367f430e9b1e4de43432870b
Siferx <127848955+Siferx@users.noreply.github.com>
2024-10-10 02:46:43 -0400

    subiendo proyecto al git

--- app/src/main/java/com/example/practicanavegacion/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/practicanavegacion/ui/activities/MainActivity.kt
@@ -9,6 +9,7 @@ import com.example.practicanavegacion.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
